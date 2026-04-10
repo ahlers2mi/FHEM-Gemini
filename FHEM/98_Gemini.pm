1ecaf47c923a6e823b3e5a741ed79a56d67e1e9d
Michael Ahlers <58474751+ahlers2mi@users.noreply.github.com>
2026-04-10 23:51:44 +0200

    Update version from 2.7.0 to 2.9.0

--- FHEM/98_Gemini.pm
+++ FHEM/98_Gemini.pm
@@ -129,7 +129,7 @@ sub Gemini_Define {
     my $name = $args[0];
     $hash->{NAME}        = $name;
     $hash->{CHAT}        = [];   # Chat-Verlauf als Array-Referenz
-    $hash->{VERSION}     = '2.7.0';
+    $hash->{VERSION}     = '2.9.0';
 
     readingsSingleUpdate($hash, 'state',             'initialized', 1);
     readingsSingleUpdate($hash, 'response',          '-',           0);
